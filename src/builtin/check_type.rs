ca958bd8254594a60f92aa550ee481cdf14f2561
Ngo Phuoc Loi <phuocloi11223@gmail.com>
2025-12-28 13:51:16 +0700

    Debug

--- src/builtin/check_type.rs
+++ src/builtin/check_type.rs
@@ -3,6 +3,7 @@ use std::collections::HashSet;
 use std::fs;
 use std::path::Path;
 use std::io;
+use std::env;
 use is_executable::IsExecutable;
 
 fn is_command_at_path(command: &str, path: &Path) -> bool {
@@ -20,31 +21,33 @@ pub fn check_type(command: &str, built_in_commands: &HashSet<&str>) -> io::Resul
         return Ok(());
     }
 
-    let paths = env!("PATH");
+    if let Some(paths) = env::var_os("PATH") {
+        for path_buf in std::env::split_paths(&paths) {
+            let p = path_buf.as_path();
+            if command == "my_exe" {
+                println!("Path: {}", p.display());
+            }
+            if p.is_file() && is_command_at_path(command, &p) {
+                return Ok(());
+            }
 
-    for path_buf in std::env::split_paths(&paths) {
-        let p = path_buf.as_path();
-        if command == "my_exe" {
-            println!("Path: {}", p.display());
-        }
-        if p.is_file() && is_command_at_path(command, &p) {
-            return Ok(());
-        }
-
-        if p.is_dir() {
-            for entry in fs::read_dir(p)? {
-                let entry = entry?;
-                let path = entry.path();
-                if command == "my_exe" {
-                    println!("\t\t SubPath: {}", path.display());
-                }
-                
-                if is_command_at_path(command, &path) {
-                    return Ok(());
+            if p.is_dir() {
+                for entry in fs::read_dir(p)? {
+                    let entry = entry?;
+                    let path = entry.path();
+                    if command == "my_exe" {
+                        println!("\t\t SubPath: {}", path.display());
+                    }
+                    
+                    if is_command_at_path(command, &path) {
+                        return Ok(());
+                    }
                 }
             }
         }
+        println!("{command}: not found");
     }
-    println!("{command}: not found");
+
+    
     Ok(())
 }
