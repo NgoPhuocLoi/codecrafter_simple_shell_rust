350524d79b5eae53ae7faa3f6310468079266574
Ngo Phuoc Loi <phuocloi11223@gmail.com>
2025-12-28 09:33:39 +0700

    Handle invalid command

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,15 @@ fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            println!("{}: command not found", input.trim());
+        },
+        Err(_) => {
+            println!("Failed to read the command!");
+        }
+    }
+    
 }
